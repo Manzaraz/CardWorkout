0540cde9b4fdeb6910cc42e6b3896393b68399bc
Christian Manzaraz <magnusmanz@gmail.com>
2024-08-12 13:01:09 -0300

    iOS  15 Update - Buttons in StoryBoard

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -11,8 +11,6 @@ class CardSelectionVC: UIViewController {
     
     @IBOutlet var cardImageView: UIImageView!
     
-    @IBOutlet var buttons: [UIButton]!
-    
     var cards: [UIImage] = Card.allValues
     
     var timer: Timer!
@@ -22,10 +20,6 @@ class CardSelectionVC: UIViewController {
         super.viewDidLoad()
         
         startTimer()
-        
-        for button in buttons {
-            button.layer.cornerRadius = 8
-        }
     }
     
     
